11bb181ea6d3405a9e47880190c00fdf3634a4a8
Shih Han <swang05@mail.bbk.ac.uk>
2018-05-21 11:32:23 +0100

    02: Account Spec | balance function

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -1,1 +1,18 @@
 'use strict';
+
+describe('Account', function(){
+
+  var account;
+
+  beforeEach(function(){
+    account = new Account();
+  });
+
+  describe('create new account', function(){
+    it('return initial balance by default', function(){
+      expect(account.balance()).toEqual(0);
+    });
+  });
+
+
+});
