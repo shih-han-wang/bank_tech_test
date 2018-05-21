edd0effe1345b7af65ccbaeda0c6f8b0f1801ed2
Shih Han <swang05@mail.bbk.ac.uk>
2018-05-21 14:51:59 +0100

    12: Account Spec | print statement function

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -45,4 +45,21 @@ describe('Account', function(){
     });
   });
 
+
+  describe('print statement', function(){
+    it('return the history statement', function(){
+      account.deposit(1000, '10/01/2012')
+      account.deposit(2000, '13/01/2012')
+      account.withdraw(500, '14/01/2012')
+      var expectOutput =
+      'date || credit || debit || balance' + "\n" +
+      '14/01/2012 ||  || 500 || 2500' + "\n" +
+      '13/01/2012 || 2000 ||  || 3000' + "\n" +
+      '10/01/2012 || 1000 ||  || 1000'
+      spyOn(console, 'log');
+      account.printStatement()
+      expect(console.log).toHaveBeenCalledWith(expectOutput);
+    });
+
+  });
 });
