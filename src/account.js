1f6478141ea649609cee378c54984f66583a4638
Shih Han <swang05@mail.bbk.ac.uk>
2018-05-21 11:33:22 +0100

    03: Account | balance function

--- src/account.js
+++ src/account.js
@@ -1,1 +1,19 @@
 'use strict';
+
+(function(exports){
+
+  function Account(INITIAL_BALAMCE = 0){
+    this._balance = INITIAL_BALAMCE
+  };
+
+  Account.prototype = {
+
+    balance: function(){
+      return this._balance
+    }
+
+  };
+
+  exports.Account = Account;
+
+})(this);
