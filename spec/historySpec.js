9d9a6ed866642b1a2e71526391527e6a77e2ac76
Shih Han <swang05@mail.bbk.ac.uk>
2018-05-22 13:49:20 +0100

    20: History Spec | add transaction function

---
+++ spec/historySpec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('History', function(){
+
+  var history;
+  var fakeTransaction = { _action: 'd', _amount: 100, _balance: 100, _date: '22/05/2018' }
+
+  beforeEach(function(){
+    history = new History();
+  });
+
+  describe('add function', function(){
+    it('push fakeTransaction object to array', function(){
+      history.add('d', 100, 100, '22/05/2018', fakeTransaction)
+      expect(history._transactions[0]).toEqual(fakeTransaction);
+    });
+  });
+
+});
