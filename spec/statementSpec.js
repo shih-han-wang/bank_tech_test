8ba22ade06ff2539e2e0f02251dbf1387c4e027c
Shih Han <swang05@mail.bbk.ac.uk>
2018-05-23 12:21:56 +0100

    28: Statement Spec | function only for display

--- spec/statementSpec.js
+++ spec/statementSpec.js
@@ -3,20 +3,29 @@
 describe('Statement', function(){
 
   var statement;
+  var fakeTransactions =
+  [ {
+      _action: 'w',
+      _amount: 500,
+      _balance: 500,
+      _date: '03/02/2022' },
+    {
+      _action: 'd',
+      _amount: 1000,
+      _balance: 1000,
+      _date: '02/02/2022' } ]
 
   beforeEach(function(){
-    statement = new Statement();
+    statement = Statement;
   });
 
   describe('print', function(){
     it('add transaction into history', function(){
-      statement.addTransaction('d', 1000, 1000, '02/02/2022')
-      statement.addTransaction('w', 500, 500, '03/02/2022')
       var expectOutput =
         'date || credit || debit || balance' + "\n" +
         '03/02/2022 ||  || 500 || 500' + "\n" +
         '02/02/2022 || 1000 ||  || 1000'
-      expect(statement.print()).toEqual(expectOutput);
+      expect(statement(fakeTransactions)).toEqual(expectOutput);
     });
   });
 
