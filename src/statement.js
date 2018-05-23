1eaaf17bbaf36510e248ce00c9a50d6a970cee3d
Shih Han <swang05@mail.bbk.ac.uk>
2018-05-23 12:22:26 +0100

    29: Statement | function only for display

--- src/statement.js
+++ src/statement.js
@@ -2,28 +2,16 @@
 
 (function(exports){
 
-  function Statement(history = new History()){
-    this._history = history;
-  };
-
-  Statement.prototype = {
-
-    addTransaction: function(action, amount, balance, date){
-      this._history.add(action, amount, balance, date)
-    },
-
-    print: function(){
-      var display = 'date || credit || debit || balance'
-      this._history._transactions.forEach(function(t){
-        if (t._action === 'd'){
-          display += ( "\n" + `${t._date} || ${t._amount} ||  || ${t._balance}` )
-        }else if(t._action === 'w'){
-          display += ( "\n" + `${t._date} ||  || ${t._amount} || ${t._balance}` )
-        }
-      });
-      return display;
-    }
-
+  function Statement(transactions){
+    var display = 'date || credit || debit || balance'
+    transactions.forEach(function(t){
+      if (t._action === 'd'){
+        display += ( "\n" + `${t._date} || ${t._amount} ||  || ${t._balance}` )
+      }else if(t._action === 'w'){
+        display += ( "\n" + `${t._date} ||  || ${t._amount} || ${t._balance}` )
+      }
+    });
+    return display;
   };
 
   exports.Statement = Statement;
