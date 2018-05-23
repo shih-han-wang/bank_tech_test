ee2b8ce48fe62855fdb0a913feb582900955b30c
Shih Han <swang05@mail.bbk.ac.uk>
2018-05-23 12:59:42 +0100

    30: Account Spec | deposit & withdraw & print statement

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -3,9 +3,27 @@
 describe('Account', function(){
 
   var account;
+  var history;
+  var statement;
+  var transactions;
 
   beforeEach(function(){
-    account = new Account();
+    history = {
+      add: function(value){
+        return value
+      },
+      _transactions:function(){
+        return []
+      }
+    };
+    transactions = {
+      forEach: function(){
+      }
+    };
+    account = new Account(0, history);
+    spyOn(history, 'add')
+    spyOn(history, '_transactions')
+    spyOn(console, 'log');
   });
 
   describe('balance', function(){
@@ -14,20 +32,26 @@ describe('Account', function(){
     });
   });
 
+  describe('deposit', function(){
+    it('return balance', function(){
+      expect(account.deposit(10)).toEqual(10);
+      expect(account._history.add).toHaveBeenCalled();
+    });
+  });
+
+  describe('withdraw', function(){
+    it('return balance', function(){
+      account.deposit(1000)
+      expect(account.withdraw(500)).toEqual(500);
+      expect(account._history.add).toHaveBeenCalled();
+    });
+  });
+
   describe('print statement', function(){
     it('return the history statement', function(){
-      account.deposit(1000, '10/01/2012')
-      account.deposit(2000, '13/01/2012')
-      account.withdraw(500, '14/01/2012')
-      var expectOutput =
-      'date || credit || debit || balance' + "\n" +
-      '14/01/2012 ||  || 500 || 2500' + "\n" +
-      '13/01/2012 || 2000 ||  || 3000' + "\n" +
-      '10/01/2012 || 1000 ||  || 1000'
-      spyOn(console, 'log');
-      account.printStatement()
-      expect(console.log).toHaveBeenCalledWith(expectOutput);
+      account.printStatement(statement, transactions)
+      expect(console.log).toHaveBeenCalled();
     });
-
   });
+  
 });
